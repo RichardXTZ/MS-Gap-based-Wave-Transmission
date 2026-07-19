9fdfb7590940cbb982a43dbc8a69e5eb5a18f9ea
RichardXTZ <tianzhi_xiang@163.com>
2026-07-19 19:03:24 +0800

    Initial

--- Main.m
+++ Main.m
@@ -10,7 +10,7 @@ addpath(pro_path);
 
 %% Acoustic Parameters
 f = 10000;
-f_del = 100;
+f_del = 50;
 c_air = 343;
 rho_air = 1.21;
 lambda_air = c_air/f;
@@ -37,47 +37,39 @@ para_bar = [rho_bar,nu_bar,E_bar];
 % const
 w_beam = 0.0012;
 
-tot_sizex = 0.016;
-cell_sizey = 0.024;
+tot_sizex = 0.03;
+cell_sizey = tot_sizex;
 
 air_sizey = lambda_air*2;
 solid_sizey = lambda_bar*2;
 
-cav_h = 0.035;
+cav_h = 0.04;
 
 minsize = 0.001;
 
 % variable
+R_tot = (cell_sizey-minsize*2)/2;
 
-%% Internal geometry limits
+R_lim = [minsize*2,tot_sizex/5];%1
+d_lim = [minsize*2,tot_sizex/5];%2
+wn_lim = [minsize,minsize*6];%2
+dc_lim = [minsize,minsize*8];%1
+prop_lim = [0.1,0.9];%NaN
 
-% cavity height
-hi_lim = [0.0030, 0.0050];
 
-% through-channel width
-ne_lim = [0.0015, 0.0030];
-
-% neck horizontal length
-wn_lim = [0.0005, 0.0015];
-
-% neck opening height
-ln_lim = [0.0008, 0.0018];
-
-% cavity horizontal width
-wi_lim = [0.0030, 0.0060];
-variale_num = 9;
+variale_num = 6;
 %% GA Parameters
 iterations = 1e4;
 population_size = 30;
-crossover_probability = 0.84;
-variation_probability = 0.1;
+crossover_probability = 0.8;
+variation_probability = 0.2;
 gene_length = 10;
 individual_size = variale_num*gene_length; 
 
 %% Data save set
-mk_path = [rootPath,'/res/result/',num2str(f),'-',num2str(cav_h),'-',num2str(tot_sizex)];
+mk_path = [rootPath,'/res/result/',num2str(f),'-',num2str(Target_pha),'-',num2str(tot_sizex)];
 da_path = [rootPath,'/res/data'];
-Target_path = [rootPath,'/res/result/',num2str(f),'-',num2str(cav_h),'-',num2str(tot_sizex),'.txt'];
+Target_path = [rootPath,'/res/result/',num2str(f),'-',num2str(Target_pha),'-',num2str(tot_sizex),'.txt'];
 mkdir(da_path);
 mkdir(mk_path);
 
